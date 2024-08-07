c064b7521ff00919ed89aa91e66d174f2dd1d848
KOS1234567890 <fistar02@gmail.com>
2024-08-07 14:48:32 +0900

    Industries.html update 2024.08.07

--- js/industries.js
+++ js/industries.js
@@ -47,3 +47,64 @@ const elOli=document.querySelectorAll('ol >li'),
 
 //  1번째 li클릭시 발동 해야할것 => 1번째 box1의 off가 추가되면서 해당 화면 감추기, 
 
+document.addEventListener('DOMContentLoaded', function() {
+    // 카테고리별 링크 선택자
+    const selectors = [
+        '.Info_macine a',
+        '.Office_macine a',
+        '.MediaEvent_macine a',
+        '.Furniture_macine a',
+        '.HomeAppliance_group a'
+    ];
+
+    // 메뉴 클릭 처리 함수
+    function handleMenuClick(event) {
+        event.preventDefault();
+        const targetClass = this.getAttribute('data-target');
+        
+        document.querySelectorAll('.main3_Plf_bt ul').forEach(ul => {
+            ul.classList.add('hidden');
+        });
+        document.querySelector(`.${targetClass}`).classList.remove('hidden');
+    }
+
+    // 모든 링크에 클릭 이벤트 리스너 추가
+    selectors.forEach(selector => {
+        document.querySelectorAll(selector).forEach(link => {
+            link.addEventListener('click', handleMenuClick);
+        });
+    });
+
+    // box1 클릭 이벤트 처리 함수
+    function handleBox1Click() {
+        // 현재 클릭된 box1의 상위 li 요소 찾기
+        const listItem = this.closest('li');
+        if (listItem) {
+            // 상위 li 내에서 main3_Pli 요소 보이기
+            const main3Pli = listItem.querySelector('.main3_Pli');
+            if (main3Pli) {
+                main3Pli.classList.remove('off');
+            }
+
+            // main3_Pli 내에서 main3_Plf_bt 찾기
+            const main3PliBt = listItem.querySelector('.main3_Plf_bt');
+            if (main3PliBt) {
+                // 모든 ul 요소에 hidden 클래스 추가
+                main3PliBt.querySelectorAll('ul').forEach(ul => {
+                    ul.classList.add('hidden');
+                });
+
+                // 가장 상단에 위치한 첫 번째 ul에서 hidden 클래스 제거
+                const firstUl = main3PliBt.querySelector('ul');
+                if (firstUl) {
+                    firstUl.classList.remove('hidden');
+                }
+            }
+        }
+    }
+
+    // 모든 box1 요소에 클릭 이벤트 리스너 추가
+    document.querySelectorAll('.box1').forEach(box1 => {
+        box1.addEventListener('click', handleBox1Click);
+    });
+});
